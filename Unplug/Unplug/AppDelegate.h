b9ac19d5c503e4b666eb660acf6e170cb92ee466
Scott Chow <tehchowster@gmail.com>
2018-01-20 12:35:48 -0800

    created xcode project.

---
+++ Unplug/Unplug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Unplug
+//
+//  Created by Scott P. Chow on 1/20/18.
+//  Copyright © 2018 sbhacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Unplug/Unplug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Unplug
+//
+//  Created by Scott P. Chow on 1/20/18.
+//  Copyright © 2018 sbhacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
