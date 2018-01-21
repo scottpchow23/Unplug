a7f4c49f1ababb847ac88c2450fdca5cc8af042c
CoolStar <coolstarorganization@gmail.com>
2018-01-21 11:29:56 -0800

    Increase delay from 0.2 to 0.5

--- Unplug/Unplug/RoomStatsViewController.m
+++ Unplug/Unplug/RoomStatsViewController.m
@@ -106,7 +106,7 @@
     __block UIBackgroundTaskIdentifier backgroundIdentifier = [[UIApplication sharedApplication] beginBackgroundTaskWithName:@"waitLock" expirationHandler:^{
         
     }];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         if (_markBackgrounded){
             NSLog(@"Marking Backgrounded");
             
